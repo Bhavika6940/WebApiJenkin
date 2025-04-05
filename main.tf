de71e0b1220cbbd3fee541c9d522fd440a30e12e
Bhavika6940 <115462254+Bhavika6940@users.noreply.github.com>
2025-04-05 14:23:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # App Service Plan
-resource "azurerm_service_plan" "plan" {
+resource "azurerm_app_service_plan" "plan" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -32,7 +32,7 @@ resource "azurerm_app_service" "app" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_service_plan.plan.id
+  app_service_plan_id = azurerm_app_service_plan.plan.id
 
   site_config {
     always_on = true
